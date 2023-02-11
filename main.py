e18a9106f0fa5979561f6c708ee51cc4abcdd3c7
jacob weiss <jacob.weiss@data-axle.com>
2023-02-11 15:51:38 -0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # TODOs:
 # 1. Look through the streamlit gallery and find a website that you want to mimic (https://streamlit.io/gallery)
 # 2. Add a file in this repo called requirements.txt
-# 3. Add streamlit to that file
+# 3. Add streamlit to requirements.txt
 # 4. Push the code to github
      # a. press the plus sign for each file (this will add it to staging)
      # b. write a commit message
